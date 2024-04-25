177bf72685d4fcf6d0ea1c99dca995f05c84ca10
Vignesh D <118509275+vigneshvikram0001@users.noreply.github.com>
2024-04-25 18:58:29 +0530

    Update App.js

--- App.js
+++ App.js
@@ -9,7 +9,6 @@ function startGame() {
     alert("Please enter your name to start the game.");
   }
 }
-
 function uniquenumber() {
   const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   let result = "";
